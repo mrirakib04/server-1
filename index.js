875c1f54780646d8cd2b471f6a5b437fa7c8c104
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-22 21:01:34 +0600

    review for update reading added

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       const result = await reviewCollection.findOne(query);
       res.send(result);
     });
+    // review for update reading
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
