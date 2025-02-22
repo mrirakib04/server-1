80aeda95435246f6c359e7aac313f6fcda607a2c
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-22 20:43:03 +0600

    riveiws reading added

---
+++ README.md
@@ -0,0 +1,34 @@
+# Server-1
+
+# For: web-practice-7 - Chill Gamer
+
+## Links:
+
+- **Web:** [https://mrirakib-web-practice-7.netlify.app/](https://mrirakib-web-practice-7.netlify.app/)
+- **Repo:** [https://github.com/mrirakib04/web-practice-7/](https://github.com/mrirakib04/web-practice-7/)
+
+## Features
+
+- **Game Reviews:**
+
+  - Fetch all reviews.
+  - Get top-rated reviews.
+  - Get reviews sorted by publish year.
+  - Fetch a review by its ID.
+  - Get reviews for a specific user.
+  - Add a new review.
+  - Update an existing review.
+  - Delete a review.
+
+- **Watchlist:**
+  - Fetch all watchlist items.
+  - Get a user's watchlist by email.
+  - Add a new item to the watchlist.
+  - Remove an item from the watchlist.
+
+## Technologies Used
+
+- **Express.js**: Web framework for Node.js to handle HTTP requests.
+- **MongoDB**: NoSQL database to store game reviews and watchlist data.
+- **CORS**: Middleware to handle cross-origin requests.
+- **dotenv**: For managing environment variables such as MongoDB credentials.
---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.bfqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+
+    // Connections
+    const database = client.db("chill_gamer");
+    const reviewCollection = database.collection("reviews");
+    const watchlistCollection = database.collection("watchlist");
+
+    // Reading
+    // reviews reading
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My HOT Server Is Ready.");
+});
+app.listen(port, () => {
+  console.log(`Server in port: ${port}`);
+});
