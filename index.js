51d3ad72137208dc490f3b6de780fc52b85e49df
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-23 00:39:03 +0600

    review deleting added

--- index.js
+++ index.js
@@ -146,6 +146,13 @@ async function run() {
       const result = await watchlistCollection.deleteOne(query);
       res.send(result);
     });
+    // review deleting
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
